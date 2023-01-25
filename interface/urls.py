42dd7f1d199e01876de746080683b3f5c8dc7f6a
Xavaki <xpavon4d@gmail.com>
2023-01-25 20:13:33 +0100

    home button

--- interface/urls.py
+++ interface/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='index'),
     path('channel/<str:channel_id>/', views.channel, name='channel'),
     path('channel/<str:channel_id>/video/<str:video_id>/', views.video, name='video')
 ]
