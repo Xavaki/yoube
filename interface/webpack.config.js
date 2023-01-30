f9c5748dbdf077ff8e5374ae32e98dd1f928f1e7
Xavaki <xpavon4d@gmail.com>
2023-01-30 23:42:01 +0100

    frontend connected to supabase

---
+++ interface/webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+    entry: {
+        index: "./static/interface/source/index.js",
+    },
+    output: {
+        path: path.resolve(__dirname, "./static/interface/frontend"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            // {
+            //     test: /\.js$/,
+            //     enforce: "pre",
+            //     use: ["source-map-loader"],
+            // },
+            {
+
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+
+                type: 'asset/resource',
+
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    // [style-loader](/loaders/style-loader)
+                    { loader: 'style-loader' },
+                    // [css-loader](/loaders/css-loader)
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    },
+                    // [sass-loader](/loaders/sass-loader)
+                    { loader: 'sass-loader' }
+                ]
+            },
+            {
+                test: /\.mp3$/,
+                loader: 'file-loader'
+            }
+        ],
+    },
+    optimization: {
+        minimize: true,
+    },
+    plugins: [
+        new webpack.DefinePlugin({
+            "process.env": {
+                // This has effect on the react lib size
+                NODE_ENV: JSON.stringify("production"),
+            },
+        }),
+    ]
+};
